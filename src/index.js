4f35372435399a976673015d992abc23bb323283
Akifumi Shimoji <shimoji-a@appleworld.work>
2020-03-30 10:14:17 +0900

    update

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={()=>{this.props.onClick()}}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
-  
 class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   renderSquare(i) {
@@ -30,8 +28,13 @@ class Board extends React.Component {
       />
     );
   }
-  hundleClick(){
-    alert()
+  hundleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares:squares,
+      xIsNext : ! this.state.xIsNext,
+    });
   }
   render() {
     const status = 'Next player: X';
